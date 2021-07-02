2a65864042cfc4e41ad9772ad6db376e74fbdcb6
Shashanka Sharma <shashankasharma@Shashankas-MBP.lan>
2021-07-01 23:48:53 -0400

    First Commit

---
+++ Project_3/SDLProject 3/SDLProject/Entity.cpp
@@ -0,0 +1,9 @@
+//
+//  Entity.cpp
+//  SDLProject
+//
+//  Created by Shashanka Sharma on 6/21/21.
+//  Copyright © 2021 ctg. All rights reserved.
+//
+
+#include "Entity.hpp"
---
+++ Project_3/SDLProject 3/SDLProject/Entity.h
@@ -0,0 +1,14 @@
+//
+//  Entity.hpp
+//  SDLProject
+//
+//  Created by Shashanka Sharma on 6/21/21.
+//  Copyright © 2021 ctg. All rights reserved.
+//
+
+#ifndef Entity_hpp
+#define Entity_hpp
+
+#include <stdio.h>
+
+#endif /* Entity_hpp */
